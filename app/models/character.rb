4eb2d789086483a07308858d71d7ede930908b79
Marnie Mycena <marniemycena@gmail.com>
2023-08-12 15:30:21 +1000

    DB schema updated, seeds created

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,5 +4,9 @@ class Character < ApplicationRecord
   belongs_to :character_class
   has_and_belongs_to_many :spells, join_table: 'character_spells'
   has_and_belongs_to_many :stats, join_table: 'character_stats'
+  serialize :weapons, Array
+  serialize :equipment, Array
+  serialize :languages, Array
 
   validates :name, :level, :race, :character_class, :max_hp, :current_hp, :armor_class, :equipment, presence: true
+end
