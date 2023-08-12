ccc0ab42dd1829a7150a40a3676b8cd6a514a87a
David Edward Lee <128865370+davidedwardlee@users.noreply.github.com>
2023-08-12 16:42:03 +1000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'characters#index'
+  root to: 'pages#home'
   
   # users
   get '/users/my_campaigns', to: 'users#campaigns', as: 'my_campaigns'
