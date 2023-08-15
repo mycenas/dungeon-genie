b176be7fdf849a094b5bfb318ac20e3d24902edb
Hieu Luong <ahomous@gmail.com>
2023-08-15 21:06:16 +1000

    starting file

---
+++ hieu's-js/hieu.js
@@ -0,0 +1,17 @@
+
+
+// create scene (layout)
+
+// create camera (position of view)
+
+// create render (colouring)
+
+// create a light (for shadows)
+
+// create animation loop
+
+// resize window (window size)
+
+
+
+// link >> <script type="module" src="FILENAME.js"></script> in html
