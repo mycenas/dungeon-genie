2394e125f07e986e8dbbad5e0dc8d3c7b29902dc
Marnie Mycena <marniemycena@gmail.com>
2023-08-17 21:35:13 +1000

    added equipment options with js

--- app/models/character.rb
+++ app/models/character.rb
@@ -12,16 +12,32 @@ class Character < ApplicationRecord
 
   validates :name, :level, :race, :character_class, :max_hp, :current_hp, :armor_class, :equipment, presence: true
 
-  def set_image_path
+    def set_image_path
         image_lookup = {
-            "tiefling_warlock" => "tiefling_warlock.png",
-            "dragonborn_fighter" => "dragonborn_fighter.png",
-            "hilldwarf_cleric" => "hilldwarf_cleric.png",
-            "woodelf_druid" => "woodelf_druid.png"
+            "male_tiefling_warlock" => "male_tiefling_warlock.png",
+            "female_tiefling_warlock" => "female_tiefling_warlock.png",
+
+            "male_woodelf_druid" => "male_woodelf_druid.png",
+            "female_woodelf_druid" => "female_woodelf_druid.png",
+
+            "male_dragonborn_fighter" => "male_dragonborn_fighter.png",
+            "female_dragonborn_fighter" => "female_dragonborn_fighter.png",
+
+            "male_hilldwarf_cleric" => "male_hilldwarf_cleric.png",
+            "female_hilldwarf_cleric" => "female_hilldwarf_cleric.png",
         }
-        key = "#{self.race.name.downcase.gsub(' ', '')}_#{self.character_class.name.downcase}"
-        self.image_path = image_lookup[key]
+
+        key_base = "#{self.race.name.downcase.gsub(' ', '')}_#{self.character_class.name.downcase}"
+
+        gender_key = self.gender.downcase
+        gender_key = ["male", "female"].sample if gender_key == 'other'
+        
+        full_key = "#{gender_key}_#{key_base}"
+
+        self.image_path = image_lookup[full_key]
     end
+
+
 
   def set_current_hp
     self.current_hp = self.max_hp
