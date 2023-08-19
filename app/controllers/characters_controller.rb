d1cd52f16c900e9b1ca43d315ae42e60f6c98bd7
Marnie Mycena <marniemycena@gmail.com>
2023-08-19 14:15:45 +1000

    Debugging infinite loop in character creation

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -15,6 +15,10 @@ class CharactersController < ApplicationController
   def create
     @character = Character.new(character_params)
     @character.user = current_user
+
+    if @character.valid?
+      set_character_stats(@character)
+
     if @character.save
       redirect_to my_characters_path, notice: "#{@character.name} was successfully created."
     else
@@ -29,8 +33,17 @@ class CharactersController < ApplicationController
 
   private
 
+  def set_character_stats(character)
+    stats_values = character.send (:stats_defaults)
+
+    stats_values.each do |name, value|
+      stat = Stats.find_or_create_by(name: name)
+      CharacterStats.create!(character: character, stats_id: stat.id, value: value )
+    end
+  end
+
   # strong parameters for character
   def character_params
-    params.require(:character).permit(:user, :name, :level, :race_id, :character_class_id, :max_hp, :current_hp, :armor_class, :gender, :languages, :equipment, :weapons)
+    params.require(:character).permit(:user, :name, :level, :race_id, :character_class_id, :max_hp, :current_hp, :armor_class, :gender, :languages, :equipment)
   end
 end
