5cb40f942b3997f4ccab2f896fd9823477a231c2
Hieu Luong <ahomous@gmail.com>
2023-08-31 19:13:00 +1000

    updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './controllers';
-import WizardController from './controllers/wizard_controller.js';
